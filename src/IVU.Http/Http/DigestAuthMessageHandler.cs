33a0fed222ebb2c73eb621ee6c50b2f183a5676b
Thomas Müller <tmueller@ivugmbh.de>
2019-12-12 15:23:48 +0100

    Fixes issue #7: NonceCount of Digest Authentication doesn't match RFC 7616 and RFC 2617 specification

--- src/IVU.Http/Http/DigestAuthMessageHandler.cs
+++ src/IVU.Http/Http/DigestAuthMessageHandler.cs
@@ -235,7 +235,7 @@
                     {
                         var ha1 = CalculateMd5Hash($"{username}:{realm}:{password}");
                         var ha2 = CalculateMd5Hash($"{method.Method}:{path}");
-                        digestResponse = CalculateMd5Hash($"{ha1}:{nonce}:{nc:X8}:{cnonce}:{qop}:{ha2}");
+                        digestResponse = CalculateMd5Hash($"{ha1}:{nonce}:{nc:x8}:{cnonce}:{qop}:{ha2}");
                     }
                     break;
 
@@ -244,7 +244,7 @@
                     {
                         var ha1 = CalculateSha256Hash($"{username}:{realm}:{password}");
                         var ha2 = CalculateSha256Hash($"{method.Method}:{path}");
-                        digestResponse = CalculateSha256Hash($"{ha1}:{nonce}:{nc:X8}:{cnonce}:{qop}:{ha2}");
+                        digestResponse = CalculateSha256Hash($"{ha1}:{nonce}:{nc:x8}:{cnonce}:{qop}:{ha2}");
                     }
                     break;
 
@@ -255,11 +255,11 @@
             if (string.IsNullOrWhiteSpace(opaque))
             {
                 return $"username=\"{username}\", realm=\"{realm}\", nonce=\"{nonce}\", uri=\"{path}\", "
-                       + $"algorithm=\"{algorithm}\", response=\"{digestResponse}\", qop=\"{qop}\", nc=\"{nc:X8}\", cnonce=\"{cnonce}\"";
+                       + $"algorithm=\"{algorithm}\", response=\"{digestResponse}\", qop=\"{qop}\", nc=\"{nc:x8}\", cnonce=\"{cnonce}\"";
             }
 
             return $"username=\"{username}\", realm=\"{realm}\", nonce=\"{nonce}\", uri=\"{path}\", "
-                       + $"algorithm=\"{algorithm}\", response=\"{digestResponse}\", qop=\"{qop}\", nc=\"{nc:X8}\", cnonce=\"{cnonce}\", opaque=\"{opaque}\"";
+                       + $"algorithm=\"{algorithm}\", response=\"{digestResponse}\", qop=\"{qop}\", nc=\"{nc:x8}\", cnonce=\"{cnonce}\", opaque=\"{opaque}\"";
         }
 
         private static string CalculateMd5Hash(string input)
