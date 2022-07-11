901fd064d4674c4fda452958dc34406f886c4b13
Thomas Müller <tmueller@ivugmbh.de>
2022-07-11 17:25:16 +0200

    Updated build script: removed 32 bit build

--- src/build.cmd
+++ src/build.cmd
@@ -70,16 +70,6 @@ rmdir /S /Q bin\dist\win7-x64\zh-Hant 2>nul
 echo.
 echo ===========================================================================
 echo.
-echo Copy precompiled Views to self-contained output
-echo.
-echo ===========================================================================
-echo.
-
-copy bin\Release\netcoreapp2.0\TRuDI.Backend.PrecompiledViews.dll bin\dist\win7-x64\TRuDI.Backend.PrecompiledViews.dll
-
-echo.
-echo ===========================================================================
-echo.
 echo Build Electron frontend
 echo.
 echo ===========================================================================
@@ -116,127 +106,7 @@ if defined CERTIFICATE_SUBJECT_NAME (
 
 if errorlevel 1 goto error
 
-echo.
-echo ===========================================================================
-echo.
-echo Rename the create file to add the target architecture to the filename
-echo.
-echo ===========================================================================
-echo.
-
-for %%x in (..\..\dist\NSIS_OUTPUT_*.exe) do (
-    set srcName="%%x" 
-	set destName="%%~nx-x86_64.exe"
-	set destName=!destName:NSIS_OUTPUT_=!
-
-	del ..\..\dist\!destName! 2>nul
-	del /q ..\..\dist\*.blockmap 2>nul
-	ren !srcName! !destName!
-)
-
-echo.
-echo ===========================================================================
-echo.
-echo Build 32-Bit version
-echo.
-echo ===========================================================================
-echo.
-
-echo.
-echo ===========================================================================
-echo.
-echo Build ASP.NET Core backend
-echo.
-echo ===========================================================================
-echo.
-
-cd ..\TRuDI.Backend
-rmdir /S /Q bin\dist 2>nul
-rmdir /S /Q ..\..\dist\win-unpacked 2>nul
-
-rem Normal build to generate the precompiled views
-dotnet build -c Release
-if errorlevel 1 goto error
-
-rem Publish the application
-dotnet publish -c Release -r win7-x86 --self-contained -o bin\dist\win7-x86 -p:SelfContainedBuild=true
-if errorlevel 1 goto error
-
-echo.
-echo ===========================================================================
-echo.
-echo Delete file not needed for TRuDI deployment
-echo.
-echo ===========================================================================
-echo.
-
-del /S bin\dist\*.pdb 2>nul
-rmdir /S /Q bin\dist\win7-x86\es 2>nul
-rmdir /S /Q bin\dist\win7-x86\fr 2>nul
-rmdir /S /Q bin\dist\win7-x86\it 2>nul
-rmdir /S /Q bin\dist\win7-x86\ja 2>nul
-rmdir /S /Q bin\dist\win7-x86\ko 2>nul
-rmdir /S /Q bin\dist\win7-x86\ru 2>nul
-rmdir /S /Q bin\dist\win7-x86\zh-Hans 2>nul
-rmdir /S /Q bin\dist\win7-x86\zh-Hant 2>nul
-
-echo.
-echo ===========================================================================
-echo.
-echo Copy precompiled Views to self-contained output
-echo.
-echo ===========================================================================
-echo.
-
-copy bin\Release\netcoreapp2.0\TRuDI.Backend.PrecompiledViews.dll bin\dist\win7-x86\TRuDI.Backend.PrecompiledViews.dll
-
-echo.
-echo ===========================================================================
-echo.
-echo Build Electron frontend
-echo.
-echo ===========================================================================
-echo.
-
-cd ..\TRuDI.Frontend
-
-echo.
-echo ===========================================================================
-echo.
-echo Generate checksums file
-echo.
-echo ===========================================================================
-echo.
-
-cmd /c node ..\Utils\createDigestList.js ..\TRuDI.Backend\bin\dist\win7-x86 checksums-win32-x86.json
-if errorlevel 1 goto error
-
-echo.
-echo ===========================================================================
-echo.
-echo Build Electron Setup
-echo.
-echo ===========================================================================
-echo.
-
-if defined CERTIFICATE_SUBJECT_NAME (
-	call node_modules\.bin\electron-builder.cmd --ia32 --config.win.certificateSubjectName="%CERTIFICATE_SUBJECT_NAME%"
-) else (
-	call node_modules\.bin\electron-builder.cmd --ia32
-)
-
-if errorlevel 1 goto error
-
-echo Rename the create file to add the target architecture to the filename
-for %%x in (..\..\dist\NSIS_OUTPUT_*.exe) do (
-    set srcName="%%x" 
-	set destName="%%~nx-x86_32.exe"
-	set destName=!destName:NSIS_OUTPUT_=!
-
-	del ..\..\dist\!destName! 2>nul
-	del /q ..\..\dist\*.blockmap 2>nul
-	ren !srcName! !destName!
-)
+del /q ..\..\dist\*.blockmap 2>nul
 
 echo.
 echo ===========================================================================
@@ -245,7 +115,8 @@ echo Build finished
 echo.
 echo ===========================================================================
 echo.
-exit 0
+pause
+rem exit 0
 
 :error
 echo.
@@ -255,4 +126,5 @@ echo Build failed!
 echo.
 echo ===========================================================================
 echo.
-exit 1
+pause
+rem exit 1
